8b9d8505d589758493b5163a5158e13b80879a7c
Tsallagov Vladimir <vovasso550@rambler.ru>
2020-11-28 06:52:39 +0300

    28.11.2020

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
 
       <div className="background">
           <div>
-              <nav className="navbar navbar-expand-lg navbar-light">
+              <nav className="navbar navbar-expand-lg navbar-light my-3">
                   <button className="navbar-toggler navbar-toggler-icons" type="button" data-toggle="collapse"
                           data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                           aria-expanded="false" aria-label="Toggle navigation">
@@ -42,19 +42,21 @@ function App() {
               </nav>
           </div>
           <div>
-
+              <hr/>
           </div>
-          <div className="text-center my-5">SCROLL DOWN TO SEE MORE</div>
-          <div className="text-center my-1">
-              <i className="fas fa-angle-double-down slimFont"></i>
+          <div className="scrollDown">
+              <div className="text-center">SCROLL DOWN TO SEE MORE</div>
+              <div className="text-center my-1">
+                  <i className="fas fa-angle-double-down slimFont"></i>
+              </div>
           </div>
       </div>
 
 
 
-      <div className="container">
+      <div className="container my-5">
           <nav className="navbar navbar-expand-lg navbar-light">
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <div className="collapse navbar-collapse">
                   <ul className="navbar-nav mr-auto">
                       <li className="nav-item active">
                           <a className="nav-links-mid-menu" href="#">HOME <span className="sr-only">(current)</span></a>
@@ -72,25 +74,32 @@ function App() {
               </div>
           </nav>
           <div>
-             <img src="" alt=""/>
+             <hr/>
           </div>
       </div>
 
 
 
       <div className="container row">
-          <div className="col-sm">
-              <img src={jasonDesktop} alt="" className="jasonMan"/>
+          <div className="col-sm text-center">
+              <img src={jasonDesktop} alt="" className="jasonMan mb-md-auto"/>
           </div>
-          <div className="col-sm">
-              <p>Lorem ipsum sagittis maecenas commodo gravida leo, malesuada orci vivamus odio, lectus ultricies sit nibh molestie. Commodo enim, adipiscing nam diam rutrum commodo rutrum sodales tempus urna morbi ultricies vitae. Gravida mattis vitae&nbsp;&mdash; gravida morbi magna: morbi mauris, in massa&nbsp;&mdash.  </p>
+          <div className="col-sm my-3">
+              <h1 className="text-left my-3">ABOUT ME</h1>
+              <p className="my-4">Lorem ipsum sagittis maecenas commodo gravida leo, malesuada orci vivamus odio, lectus ultricies sit nibh molestie. Commodo enim, adipiscing nam diam rutrum commodo rutrum sodales tempus urna morbi ultricies vitae. Gravida mattis vitae&nbsp;&mdash; gravida morbi magna: morbi mauris, in massa&nbsp;&mdash.  </p>
               <p>Ipsum vivamus, justo metus, lorem sed sodales et bibendum porttitor eu tellus congue ut vulputate ligula. Vivamus; nam enim mauris pharetra porttitor orci amet eu quisque cursus.  </p>
+              <h4>Jason Wood</h4>
           </div>
       </div>
 
 
 
-      <div></div>
+      <div className="my-5">
+          <div className="text-center greyColorText">KEEP SCROLLING, THERE IS STILL MORE TO COME</div>
+          <div className="text-center my-1 greyColorText">
+              <i className="fas fa-angle-double-down"></i>
+          </div>
+      </div>
     </div>
   );
 }
