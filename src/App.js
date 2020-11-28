595f2b33eaf4aa481dbbdf1e80e4f383a6d7fb28
Tsallagov Vladimir <vovasso550@rambler.ru>
2020-11-28 03:05:59 +0300

    28.11.2020

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import logo from './pictures/menuMobile.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+          <div>
+              <nav className="navbar navbar-expand-lg navbar-light">
+                  <button className="navbar-toggler navbar-toggler-icons" type="button" data-toggle="collapse"
+                          data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                          aria-expanded="false" aria-label="Toggle navigation">
+                      <span className="menuToggle">
+                          <img src={logo} alt=""/>
+                          MENU
+                      </span>
+                  </button>
+
+                  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                      <ul className="navbar-nav mr-auto">
+                          <li className="nav-item active">
+                              <a className="nav-links" href="#">HOME <span className="sr-only">(current)</span></a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-links" href="#">ABOUT ME</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-links" href="#">PORTFOLIO</a>
+                          </li>
+                          <li className="nav-item">
+                              <a className="nav-links" href="#">CONTACT</a>
+                          </li>
+                      </ul>
+                  </div>
+              </nav>
+          </div>
+          <div>SCROLL</div>
+          <div>ARROW</div>
+      </div>
+      <div></div>
+      <div></div>
+      <div></div>
     </div>
   );
 }
